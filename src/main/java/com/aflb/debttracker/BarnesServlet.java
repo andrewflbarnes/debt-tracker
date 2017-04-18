15346b28ea39bf94e3e2ec324179b2167f224998
barnesly <andrewflbarnes@hotmail.co.uk>
2017-04-18 22:34:03 +0100

    DT000 - Initial commit

---
+++ src/main/java/com/aflb/debttracker/BarnesServlet.java
@@ -0,0 +1,15 @@
+package com.aflb.debttracker;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BarnesServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().println("{ \"servlet\": \"" + BarnesServlet.class.toString() + "\" }");
+  }
+}
