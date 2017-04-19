350ed208c603c5c0ce3aff3db7bc668e1981f8c6
barnesly <andrewflbarnes@hotmail.co.uk>
2017-04-20 00:47:23 +0100

    DT000 - Added javadocs to AccountingDao

--- src/main/java/com/aflb/debttracker/dao/AccountingDao.java
+++ src/main/java/com/aflb/debttracker/dao/AccountingDao.java
@@ -12,8 +12,42 @@ import com.aflb.debttracker.accounting.AccountEntryType;
  *         DAO for accessing persisted {@link AccountEntry}s.
  */
 public interface AccountingDao {
+	/**
+	 * Get {@link AccountEntry}s for a specific {@code user}
+	 * 
+	 * @param user
+	 *            The user to retrieve {@link AccountEntry}s for
+	 * @return A list of {@link AccountEntry}s belonging to the {@code user}
+	 */
 	List<AccountEntry> getEntries(String user);
+
+	/**
+	 * Get {@link AccountEntry}s of the specific {@link AccountEntryType}
+	 * 
+	 * @param type
+	 *            The {@link AccountEntryType} to retrieve {@link AccountEntry}s
+	 *            of
+	 * @return A list of {@link AccountEntry}s of the type {@code type}
+	 */
 	List<AccountEntry> getEntries(AccountEntryType type);
+
+	/**
+	 * Get {@link AccountEntry}s from a specific {@code date}
+	 * 
+	 * @param date
+	 *            The {@code date} to retrieve {@link AccountEntry}s from
+	 * @return A list of {@link AccountEntry}s from the {@code date}
+	 */
 	List<AccountEntry> getEntries(Date date);
-	List<AccountEntry> getEntries(AccountingDaoFilter helper);
+
+	/**
+	 * Get {@link AccountEntry}s using a {@link AccountingDaoFilter}
+	 * 
+	 * @param filter
+	 *            The {@link AccountingDaoFilter} to retrieve
+	 *            {@link AccountEntry}s on.
+	 * @return A list of {@link AccountEntry}s matching the
+	 *         {@link AccountingDaoFilter}
+	 */
+	List<AccountEntry> getEntries(AccountingDaoFilter filter);
 }
